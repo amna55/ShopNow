b7303d759295fe359121907a581619cf65400e4a
Amna Saad <106149828+amna55@users.noreply.github.com>
2025-09-19 11:01:08 +0200

    new

--- app/composables/useSupabase.ts
+++ app/composables/useSupabase.ts
@@ -1,18 +1,22 @@
-import { createClient, type SupabaseClient } from "@supabase/supabase-js"
+// composables/useSupabase.ts
+import { createClient, type SupabaseClient } from '@supabase/supabase-js'
 
 let supabase: SupabaseClient | null = null
 
 export const useSupabase = (): SupabaseClient => {
-  if (supabase) return supabase
+    if (supabase) return supabase
 
-  if (import.meta.client) { // <-- updated
-    const config = useRuntimeConfig()
-    supabase = createClient(
-      config.public.SUPABASE_URL as string,
-      config.public.SUPABASE_KEY as string
-    )
-    return supabase
-  }
+    if (import.meta.client) {
+        const config = useRuntimeConfig()
+        if (!config.public.SUPABASE_URL || !config.public.SUPABASE_KEY)
+            throw new Error('Supabase URL or Key is missing')
 
-  throw new Error("Supabase client is only available on client-side")
+        supabase = createClient(
+            config.public.SUPABASE_URL as string,
+            config.public.SUPABASE_KEY as string
+        )
+        return supabase
+    }
+
+    throw new Error('Supabase client is only available on client-side')
 }
